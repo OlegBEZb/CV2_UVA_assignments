07d13981eb1fd34e5295b5a25415985fdaae04f8
Oleg Litvinov <olegolego@mail.ru>
2022-04-17 23:17:29 +0200

    adapted the tutorial

---
+++ assignment1/src/data_utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+import open3d as o3d
+
+
+# == Load data ==
+def open3d_example():
+    pcd = o3d.io.read_point_cloud("Data/data/0000000000.pcd")
+    # ## convert into ndarray
+
+    pcd_arr = np.asarray(pcd.points)
+
+    # ***  you need to clean the point cloud using a threshold ***
+    pcd_arr_cleaned = pcd_arr
+
+    # visualization from ndarray
+    vis_pcd = o3d.geometry.PointCloud()
+    vis_pcd.points = o3d.utility.Vector3dVector(pcd_arr_cleaned)
+    o3d.visualization.draw_geometries([vis_pcd])
+
+
+def open_wave_data(DATA_DIR):
+    target = np.load(os.path.join(DATA_DIR, 'wave_target.npy'))
+    source = np.load(os.path.join(DATA_DIR, 'wave_source.npy'))
+    return source, target
+
+
+def open_bunny_data(DATA_DIR):
+    target = np.load(os.path.join(DATA_DIR, 'bunny_target.npy'))
+    source = np.load(os.path.join(DATA_DIR, 'bunny_source.npy'))
+    return source, target
