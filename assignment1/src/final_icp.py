2ef910be35e383d86d5a5087f47809a54256353f
Oleg Litvinov <olegolego@mail.ru>
2022-04-22 23:34:29 +0200

    added evaluation

--- assignment1/src/final_icp.py
+++ assignment1/src/final_icp.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import open3d as o3d
 import os
@@ -52,6 +53,7 @@ def icp(source, target, method='bf', sampling='none', epsilon=0.0001, n_space=3,
 
     RMSE_old = np.inf
     RMSE = 100  # just a initial value
+    RMSE_list = []
     # RMSE = mean_squared_error(source, target, squared=False)
     print('Initial RMSE:', RMSE)
 
@@ -59,6 +61,7 @@ def icp(source, target, method='bf', sampling='none', epsilon=0.0001, n_space=3,
     target_updated = deepcopy(target)
 
     # unless RMS is unchanged(<= epsilon)
+    iter = 0
     while np.abs(RMSE_old - RMSE) > epsilon and RMSE != 0.0:
         RMSE_old = RMSE
 
@@ -71,7 +74,10 @@ def icp(source, target, method='bf', sampling='none', epsilon=0.0001, n_space=3,
             X = int(np.log(source.shape[1] / source_updated.shape[1]) / np.log(4))  # number of times already divided by 4
             if source_updated.shape[1] / 4 > 50:
                 modified_source = apply_R_t_lists(source.copy(), R_list, t_list)
-                source_updated, target_updated = uniform_sampling(modified_source, target.copy(), part=4 ** (X + 1))
+                source_updated, target_updated = uniform_sampling(modified_source, target.copy(), take_n=4 ** (X + 1))
+                print('after multires')
+                print('source_updated', source_updated.shape)
+                print('target_updated', target_updated.shape)
 
         # 3. transform point cloud with R and t
         transformed = (source_updated.T @ R + t).T
@@ -86,6 +92,7 @@ def icp(source, target, method='bf', sampling='none', epsilon=0.0001, n_space=3,
 
         # 5. Calculate RMSE
         RMSE = mean_squared_error(matched, transformed, squared=False)
+        RMSE_list.append(RMSE)
         print('RMSE:', RMSE)
         print('RMSE with original:', mean_squared_error(source_updated, transformed, squared=False))
 
@@ -98,7 +105,11 @@ def icp(source, target, method='bf', sampling='none', epsilon=0.0001, n_space=3,
 
         source_updated = transformed
 
-    return transformed, R_list, t_list
+        iter += 1
+        if iter >= 15:
+            break
+
+    return transformed, R_list, t_list, RMSE_list
 
 
 def calc_transformation(source, target):
@@ -157,8 +168,8 @@ def apply_R_t_lists(source, R_list, t_list):
     return source_sequential
 
 
-def uniform_sampling(source, target, part=50):
-    c = int(min(source.shape[1], target.shape[1]) / part)
+def uniform_sampling(source, target, take_n=1000):
+    c = min(source.shape[1], target.shape[1], take_n)
     source = np.random.permutation(source.T)[:c, :].T
     target = np.random.permutation(target.T)[:c, :].T
     return source, target
@@ -180,58 +191,91 @@ def uniform_sampling(source, target, part=50):
 ############################
 
 if __name__ == "__main__":
-    SAMPLING_METHOD = 'multires'  # all, uni, rand, multires, grad
+    import pandas as pd
+    res_df = pd.DataFrame(columns=['icp_time', 'method', 'sampling', 'epsilon', 'rmse_list'])
+    for i in range(5):
+        for sampling_method in [
+            'all',
+            'uni', 'rand', 'multires',
+            'grad'
+        ]:
+            for matching_method in [
+                'bf',
+                'kd'
+            ]:
+                for epsilon in [
+                    1e-3,
+                    # 1e-4,
+                                1e-5, 1e-6, 1e-7
+                                ]:
+                    print('running', sampling_method, matching_method, epsilon)
 
-    orig_bunny_source, orig_bunny_target = open_bunny_data()
-    bunny_source, bunny_target = orig_bunny_source.copy(), orig_bunny_target.copy()
+                    orig_bunny_source, orig_bunny_target = open_bunny_data()
+                    bunny_source, bunny_target = orig_bunny_source.copy(), orig_bunny_target.copy()
 
-    if SAMPLING_METHOD == 'all':
-        # downsampling both point clouds for the brute force search for correspondences
-        step = 1 # 100
-        bunny_source = bunny_source[:, ::step]
-        bunny_target = bunny_target[:, ::step]
+                    start = time.time()
 
-        # The most straight-forward approach to deal with number mismatch between source and target
-        if bunny_source.shape != bunny_target.shape:
-            so = bunny_source.shape
-            ta = bunny_target.shape
+                    if sampling_method == 'all':
+                        if matching_method == 'bf':
+                            bunny_source, bunny_target = uniform_sampling(bunny_source, bunny_target, take_n=1500)
 
-            # TODO: replace with sampling
-            if (so[1] < ta[1]):
-                bunny_target = bunny_target[:, :so[1]]
-            elif so[1] > ta[1]:
-                bunny_source = bunny_source[:, :ta[1]]
-    elif SAMPLING_METHOD == 'uni':
-        # downsampling both point clouds using uniform sampling
-        bunny_source, bunny_target = uniform_sampling(bunny_source, bunny_target, part=50)
-    elif SAMPLING_METHOD == 'grad':
-        grad = np.gradient(bunny_source, axis=1)  # x,y,z together along all the points
-        grad = np.linalg.norm(grad, ord=2, axis=0)  # for each point we have grad
-        quartile = np.percentile(grad, 75)  # Q1
-        high_grad_idx_src = np.squeeze(np.argwhere(grad >= quartile))
-        bunny_source = bunny_source[:, high_grad_idx_src]
+                        # The most straight-forward approach to deal with number mismatch between source and target
+                        if bunny_source.shape != bunny_target.shape:
+                            bunny_source, bunny_target = uniform_sampling(bunny_source, bunny_target, take_n=np.inf)
 
-        # for target we just take first n max grad points, whene n is the number of points for source
-        grad = np.gradient(bunny_target, axis=1)  # x,y,z together along all the points
-        grad = np.linalg.norm(grad, ord=2, axis=0)  # for each point we have grad
-        high_grad_idx_tgt = grad.argsort()[::-1][:len(high_grad_idx_src)]
-        bunny_target = bunny_target[:, high_grad_idx_tgt]
+                    elif sampling_method in ['uni', 'multires']:
+                        # downsampling both point clouds using uniform sampling
+                        bunny_source, bunny_target = uniform_sampling(bunny_source, bunny_target, take_n=1000)
+                    elif sampling_method == 'grad':
+                        grad = np.gradient(bunny_source, axis=1)  # x,y,z together along all the points
+                        grad = np.linalg.norm(grad, ord=2, axis=0)  # for each point we have grad
+                        # quartile = np.percentile(grad, 75)  # Q3
+                        # high_grad_idx_src = np.squeeze(np.argwhere(grad >= quartile))
+                        high_grad_idx_src = grad.argsort()[::-1][:1000]
+                        bunny_source = bunny_source[:, high_grad_idx_src]
 
-    source = get_point_cloud(bunny_source.T, [1, 0, 0])  # red
-    target = get_point_cloud(bunny_target.T, [0, 1, 0])  # green
-    o3d.visualization.draw_geometries([target, source])
+                        # for target we just take first n max grad points, whene n is the number of points for source
+                        grad = np.gradient(bunny_target, axis=1)  # x,y,z together along all the points
+                        grad = np.linalg.norm(grad, ord=2, axis=0)  # for each point we have grad
+                        high_grad_idx_tgt = grad.argsort()[::-1][:len(high_grad_idx_src)]
+                        bunny_target = bunny_target[:, high_grad_idx_tgt]
 
-    transformed, R_list, t_list = icp(source=bunny_source, target=bunny_target, method='kd',
-                                      sampling=SAMPLING_METHOD, epsilon=0.00001, n_space=3, part=50)
+                    print('bunny_source', bunny_source.shape)
+                    print('bunny_target', bunny_target.shape)
 
-    constructed_comb = apply_R_t_lists(orig_bunny_source.copy(), R_list, t_list)  # make the same as the last iteration
-    constructed_comb = get_point_cloud(constructed_comb.T, [1, 0, 1])  # purple
+                    # source = get_point_cloud(bunny_source.T, [1, 0, 0])  # red
+                    # target = get_point_cloud(bunny_target.T, [0, 1, 0])  # green
+                    # o3d.visualization.draw_geometries([target, source])
 
-    source = get_point_cloud(orig_bunny_source.T, [1, 0, 0])  # red
-    target = get_point_cloud(orig_bunny_target.T, [0, 1, 0])  # green
 
-    transformed = get_point_cloud(transformed.T, [0, 0, 1])  # blue
-    o3d.visualization.draw_geometries([target, source,
-                                       constructed_comb,
-                                       transformed
-                                       ])
+
+                    transformed, R_list, t_list, RMSE_list = icp(source=bunny_source, target=bunny_target,
+                                                                 method=matching_method,
+                                                                 sampling=sampling_method,
+                                                                 epsilon=epsilon, n_space=3,
+                                                                 take_n=1000)
+
+                    end = time.time()
+                    icp_time = end - start
+
+                    # constructed_comb = apply_R_t_lists(orig_bunny_source.copy(), R_list, t_list)  # make the same as the last iteration
+                    # constructed_comb = get_point_cloud(constructed_comb.T, [1, 0, 1])  # purple
+                    #
+                    # source = get_point_cloud(orig_bunny_source.T, [1, 0, 0])  # red
+                    # target = get_point_cloud(orig_bunny_target.T, [0, 1, 0])  # green
+                    #
+                    # transformed = get_point_cloud(transformed.T, [0, 0, 1])  # blue
+                    # o3d.visualization.draw_geometries([target, source,
+                    #                                    constructed_comb,
+                    #                                    transformed
+                    #                                    ])
+
+                    results = {'icp_time': icp_time,
+                               'method': matching_method,
+                               'sampling': sampling_method,
+                               'epsilon': epsilon,
+                               'rmse_list': RMSE_list}
+                    print('results', results)
+                    res_df = res_df.append(results, ignore_index=True)
+                    print('res_df\n', res_df)
+                    res_df.to_csv('results_tolerance.csv', index=False)
